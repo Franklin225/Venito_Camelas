36bc98ad79d1f331fc07ee9e74e7973f5c285262
Juan Velez <juane8w@gmail.com>
2024-03-13 21:01:45 -0500

    Se agrega la logica Base

--- LogicaProporcional.py
+++ LogicaProporcional.py
@@ -1,1 +1,9 @@
-print("hola" * 8)
+import ttg
+
+proposiciones = ['p', 'q', 'r']
+
+expresionLogica = ['(p implies q)', '(not q and r)', '(r implies q)']
+
+tablita = ttg.Truths(proposiciones, expresionLogica)
+
+print(tablita)
