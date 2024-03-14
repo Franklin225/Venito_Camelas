905522766b3d95a879e1aecaa64e9620c3c41055
Juan Velez <mrjiuby@gmail.com>
2024-03-13 21:47:39 -0500

    Se corrigen algunas cosas (nose)

---
+++ MatematicasDiscretas/LogicaProporcional.py
@@ -0,0 +1,41 @@
+import ttg
+
+proposiciones = ['p', 'q', 'r']
+
+
+expresionLogica = ['p implies q', 'not q', 'not q and r', '(p implies q) or (not q and r)', 'r implies q', 'p implies q', '((p implies q) or (not q and r)) implies (r implies q)']
+
+tablita = ttg.Truths(proposiciones, expresionLogica)
+
+print(tablita)
+
+"""
+De alguna forma no me funciona el pip freeze asi que adjunto el link para descargar la libreria
+https://pypi.org/project/truth-table-generator/
+
+El codigo depende mas que todo de que coloquen bien las expresiones logicas, ya que si no lo hacen, el codigo no funcionara como deberia
+
+El anterior codigo es la respuesta del ejercicio anteriormente planteado en clase
+https://photos.app.goo.gl/pBzffudiEvhAQLE57
+
+Muito cuidado pq el codigo esta al reves
+"""
+
+"""
+Este otro codigo es la version volteada del anterior como lo planteo el profesor
+import ttg
+
+proposiciones = ['p', 'q', 'r']
+
+expresionLogica = ['p implies q', 'not q', 'not q and r', '(p implies q) or (not q and r)', 'r implies q', 'p implies q', '((p implies q) or (not q and r)) implies (r implies q)']
+
+tablita = ttg.Truths(proposiciones, expresionLogica)
+
+tablita_list = str(tablita).split('\n')
+
+tablita_list = tablita_list[::-1]
+
+tablita_invertida = '\n'.join(tablita_list)
+
+print(tablita_invertida)
+"""
